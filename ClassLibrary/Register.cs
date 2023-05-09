18e02288cab134f8ca8e65c768ede733c4ea02e6
Sayiki <arzaqajradika1@gmail.com>
2023-05-09 16:24:40 +0700

    Register and PointCounter

---
+++ ClassLibrary/Register.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArzaqLibrary;
+
+public class Register<T>
+{
+    // Input variables
+    public T username { get; set; }
+    public T password { get; set; }
+    public T confirmPassword { get; set; }
+    public T email { get; set; }
+
+    // Constructor Register
+    public Register(T username, T password, T confirmPassword, T email)
+    {
+        this.username = username;
+        this.password = password;
+        this.confirmPassword = confirmPassword;
+        this.email = email;
+    }
+
+    // Registration method
+    public void RegisterUser()
+    {
+        // Check if all fields are filled
+        if (string.IsNullOrEmpty(username.ToString()) || string.IsNullOrEmpty(password.ToString()) ||
+            string.IsNullOrEmpty(confirmPassword.ToString()) || string.IsNullOrEmpty(email.ToString()))
+        {
+            Console.WriteLine("Please fill in all fields.");
+        }
+        // Check if password and confirm password match
+        else if (!password.Equals(confirmPassword))
+        {
+            Console.WriteLine("Passwords do not match.");
+        }
+        // Check if email is valid
+        else if (!IsValidEmail(email.ToString()))
+        {
+            Console.WriteLine("Invalid email address.");
+        }
+        else
+        {
+            // Register user
+            Console.WriteLine("User registered successfully.");
+            Console.WriteLine("Username: " + username);
+            Console.WriteLine("Email: " + email);
+        }
+    }
+
+    // Defensive programming
+    static bool IsValidEmail(string email)
+    {
+        try
+        {
+            var addr = new System.Net.Mail.MailAddress(email);
+            return addr.Address == email;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+}
--- EcoRecycle/Program.cs
+++ EcoRecycle/Program.cs
@@ -1,2 +1,32 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using ArzaqLibrary;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Get user input
+        Console.WriteLine("Enter username: ");
+        string username = Console.ReadLine();
+
+        Console.WriteLine("Enter password: ");
+        string password = Console.ReadLine();
+
+        Console.WriteLine("Confirm password: ");
+        string confirmPassword = Console.ReadLine();
+
+        Console.WriteLine("Enter email: ");
+        string email = Console.ReadLine();
+
+        // Create registration form with appropriate data types
+        Register<string> regForm = new Register<string>(username, password, confirmPassword, email);
+
+        // Register user
+        regForm.RegisterUser();
+
+        // Wait for user input before closing console window
+        Console.WriteLine("Press any key to exit.");
+        Console.ReadKey();
+    }
+}
