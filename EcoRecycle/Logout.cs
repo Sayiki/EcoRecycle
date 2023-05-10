9967d69d7c0506cbb8585ebc9e5eb81396691950
Bengley123 <yesasihombing1@gmail.com>
2023-05-10 13:31:27 +0700

    Menambahkan logout dan MaterialCounter

---
+++ EcoRecycle/Logout.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TUBES_KPL_2023
+{
+    internal class Logout
+    {
+        public enum State
+        {
+            Idle,
+            Confirming,
+            LoggingOut
+        }
+
+        public State currentState = State.Idle;
+
+        public void ProcessInput(string input)
+        {
+            switch (currentState)
+            {
+                case State.Idle:
+       
+                    if (input == "logout")
+                    {
+                        Console.WriteLine("Apakah anda yakin ingin logout? (y/n)");
+                        currentState = State.Confirming;
+                    }
+                    break;
+
+                case State.Confirming:
+                    if (input == "y")
+                    {
+                        Console.WriteLine("Anda telah logout.");
+                        currentState = State.LoggingOut;
+                    }
+                    else if (input == "n")
+                    {
+                        Console.WriteLine("Logout dibatalkan.");
+                        currentState = State.Idle;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Masukkan y atau n.");
+                    }
+                    break;
+
+                case State.LoggingOut:
+                    break;
+            }
+        }
+    }
+}
--- EcoRecycle/Program.cs
+++ EcoRecycle/Program.cs
@@ -1,61 +1,46 @@
-﻿using KapasitasVendingMachine;
+﻿using System;
+using System.Collections.Generic;
+using TUBES_KPL_2023;
 
-public class Program
-{
-    private static void Main(string[] args)
-    {
-        //homescreen
-        Console.WriteLine("-----EcoRecycle-----");
-        Console.WriteLine("Selamat Datang user");
-        Console.WriteLine(" ");
-
-        Console.WriteLine("Menu");
-        Console.WriteLine("1.Jenis sampah di vending machine");
-        Console.WriteLine("2.Edit profil");
-        Console.WriteLine("3.Lihat Poin");
-        Console.WriteLine("4.Kapasitas vending machine");
-
-        try
+class Program
+{   
+        static void Main(string[] args)
         {
-            Console.WriteLine("Masukkan nomor menu : ");
-            string inputMenu = Console.ReadLine();
-            switch (inputMenu)
+        // Membuat objek MaterialCounter
+        MaterialCounter counter = new MaterialCounter();
+
+        // Meminta pengguna memasukkan jenis sampah yang masuk, ketik 'exit' untuk keluar
+        string ans = "";
+        while (ans != "exit")
+        {
+            Console.Write("Masukkan sampah yang masuk (ketik 'exit' untuk keluar): ");
+            ans = Console.ReadLine();
+
+            if (ans != "exit")
             {
-                case "1":
-                    Console.WriteLine("Jenis sampah di vending machine");
-                    break;
-                case "2":
-                    Console.WriteLine("Edit profil");
-                    break;
-                case "3":
-                    Console.WriteLine("Lihat poin");
-                    break;
-                case "4":
-                    Console.WriteLine("Kapasitas vending machine: ");
-                    Sampah.inputSampah<string>("botol kaca");
-                    Sampah.inputSampah<string>("kaleng");
-                    Sampah.inputSampah<string>("botol plastik");
-                    Kapasitas.totalKapasitas();
-                    break;
-                default:
-                    Console.WriteLine("Nomor menu tidak tersedia");
-                    break;
+                // Memanggil method Count pada objek MaterialCounter
+                counter.Count(ans);
             }
         }
-        catch(Exception ex)
-        {
-            Console.WriteLine(ex.Message);
-        }
-    }
 
-    class Sampah
-    {
-        public static void inputSampah<T>(T input)
-        {
-            for(int i = 0; i <= 6; i++)
+        // Menampilkan hasil jumlah sampah
+        counter.Display();
+
+
+
+        // Tunggu user menekan tombol sebelum menutup program
+        Console.ReadKey();
+            Logout logout = new Logout();
+                string input;
+
+            do
             {
-                string masukkanSampah = Console.ReadLine();
-            }
+                Console.Write("Masukkan input: ");
+                input = Console.ReadLine();
+                logout.ProcessInput(input);
+
+            } while (logout.currentState != Logout.State.LoggingOut);
         }
-    }
+    
 }
+
