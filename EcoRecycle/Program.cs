d8151dce0b7c222c6606763366087158ccfc9525
hermwnS <126874286+hermwnS@users.noreply.github.com>
2023-05-11 18:08:22 +0700

    merubah isi kode kapasitas vending machine

--- EcoRecycle/Program.cs
+++ EcoRecycle/Program.cs
@@ -1,5 +1,5 @@
 ﻿using KapasitasVendingMachine;
-
+using System.Configuration;
 public class Program
 {
     private static void Main(string[] args)
@@ -23,18 +23,27 @@ public class Program
             {
                 case "1":
                     Console.WriteLine("Jenis sampah di vending machine");
+                    List<string> sampahList = new List<string> { "Organik", "Anorganik", "Sampah Berbahaya" };
+                    foreach(string sampah in sampahList)
+                    {
+                        Console.WriteLine(sampah);
+                    }
                     break;
                 case "2":
                     Console.WriteLine("Edit profil");
+                    //EditProfile();
                     break;
                 case "3":
                     Console.WriteLine("Lihat poin");
                     break;
                 case "4":
                     Console.WriteLine("Kapasitas vending machine: ");
-                    Sampah.inputSampah<string>("botol kaca");
-                    Sampah.inputSampah<string>("kaleng");
-                    Sampah.inputSampah<string>("botol plastik");
+                    Sampah.inputSampah<string>();
+                    Sampah.inputSampah<string>();
+                    Sampah.inputSampah<string>();
+                    Sampah.inputSampah<string>();
+                    Sampah.inputSampah<string>();
+                    Sampah.inputSampah<string>();
                     Kapasitas.totalKapasitas();
                     break;
                 default:
@@ -50,12 +59,31 @@ public class Program
 
     class Sampah
     {
-        public static void inputSampah<T>(T input)
+        public static void inputSampah<T>()
         {
-            for(int i = 0; i <= 6; i++)
-            {
-                string masukkanSampah = Console.ReadLine();
-            }
+            string masukkanSampah = Console.ReadLine();
         }
     }
+
+    /*private static void EditProfile()
+    {
+        Console.WriteLine("Silahkan masukkan data baru");
+        
+        Console.WriteLine("Nama: ");
+        string nama = Console.ReadLine();
+
+        Console.WriteLine("Alamant: ");
+        string alamat = Console.ReadLine();
+
+        Console.WriteLine("Nomor Telepon: ");
+        string nomorTelepon = Console.ReadLine();
+
+        Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+        config.AppSettings.Settings["nama"].Value = nama;
+        config.AppSettings.Settings["alamat"].Value = alamat;
+        config.AppSettings.Settings["nomorTelepon"].Value = nomorTelepon;
+        config.Save(ConfigurationSaveMode.Modified);
+
+        Console.WriteLine("Data profil berhasil diubah");
+    }*/
 }
