ee02c6727bd7483ce402f728e2bdd1a3ce365e53
Dhafff <127295164+Dhafff@users.noreply.github.com>
2023-05-14 22:34:52 +0700

    update print list sampah and edit profil

--- EcoRecycle/Program.cs
+++ EcoRecycle/Program.cs
@@ -1,4 +1,8 @@
-﻿using KapasitasVendingMachine;
+﻿using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Reflection;
 
 public class Program
 {
@@ -14,7 +18,8 @@ public class Program
         Console.WriteLine("2.Edit profil");
         Console.WriteLine("3.Lihat Poin");
         Console.WriteLine("4.Kapasitas vending machine");
-
+        Console.WriteLine("5.Logout");
+    
         try
         {
             Console.WriteLine("Masukkan nomor menu : ");
@@ -23,39 +28,90 @@ public class Program
             {
                 case "1":
                     Console.WriteLine("Jenis sampah di vending machine");
+                    List<string> sampahList = new List<string> { "Organik", "Anorganik", "Sampah Berbahaya" };
+                    foreach (string sampah in sampahList)
+                    {
+                        Console.WriteLine(sampah);
+                    }
                     break;
                 case "2":
                     Console.WriteLine("Edit profil");
+                    EditProfil();
                     break;
                 case "3":
                     Console.WriteLine("Lihat poin");
                     break;
                 case "4":
                     Console.WriteLine("Kapasitas vending machine: ");
-                    Sampah.inputSampah<string>("botol kaca");
-                    Sampah.inputSampah<string>("kaleng");
-                    Sampah.inputSampah<string>("botol plastik");
-                    Kapasitas.totalKapasitas();
+                    break;
+                case "5":
+                    Console.WriteLine("Logout");
                     break;
                 default:
                     Console.WriteLine("Nomor menu tidak tersedia");
                     break;
             }
         }
-        catch(Exception ex)
+        catch (Exception ex)
         {
             Console.WriteLine(ex.Message);
         }
     }
 
-    class Sampah
+    private static void EditProfil()
     {
-        public static void inputSampah<T>(T input)
+        try
         {
-            for(int i = 0; i <= 6; i++)
-            {
-                string masukkanSampah = Console.ReadLine();
-            }
+            // Load configuration from appsettings.json file
+            IConfiguration configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .Build();
+
+            // Read current profile data from configuration
+            string name = configuration.GetSection("Profile:Nama").Value;
+            string email = configuration.GetSection("Profile:Email").Value;
+            string password = configuration.GetSection("Profile:Password").Value;
+
+            // Display current profile data
+            Console.WriteLine("Profil kamu sekarang:");
+            Console.WriteLine($"Nama: {name}");
+            Console.WriteLine($"Email: {email}");
+            Console.WriteLine($"Password: {password}");
+
+            // Prompt user to enter new profile data
+            Console.Write("Masukkan nama baru: ");
+            string newName = Console.ReadLine();
+            Console.Write("Masukkan email baru: ");
+            string newEmail = Console.ReadLine();
+            Console.Write("Masukkan password baru: ");
+            string newPassword = Console.ReadLine();
+
+            // Update profile data in configuration
+            configuration.GetSection("Profile:Name").Value = newName;
+            configuration.GetSection("Profile:Email").Value = newEmail;
+            configuration.GetSection("Profile:Password").Value = newPassword;
+
+            // Save configuration changes to appsettings.json file
+            var json = JsonConvert.SerializeObject(configuration.GetChildren().ToList(), Formatting.Indented);
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "appsettings.json");
+
+
+            Console.WriteLine();
+            Console.WriteLine("Data berhasil di ubah!");
+
+            Console.WriteLine("Profil kamu sekarang:");
+            Console.WriteLine($"Nama: {newName}");
+            Console.WriteLine($"Email: {newEmail}");
+            Console.WriteLine($"Password: {newPassword}");
+
+            File.WriteAllText(path, json);
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
         }
     }
+
 }
