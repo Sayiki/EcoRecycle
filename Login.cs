3a42d7bccb255fa2193daeee425db5510148e3c2
Triani Putri Mumpuni <127008970+trianipm@users.noreply.github.com>
2023-06-05 00:48:20 +0700

    login gui1

---
+++ Login.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace GUI
+{
+    public partial class Login : Form
+    {
+        private Dictionary<string, string> userCredentials; // Dictionary to store user credentials
+
+        public Login()
+        {
+            InitializeComponent();
+
+            // Initialize user credentials
+            InitializeUserCredentials();
+        }
+
+        private void InitializeUserCredentials()
+        {
+            // Initialize user credentials using table-driven approach
+            userCredentials = new Dictionary<string, string>()
+            {
+                { "admin", "password123" },
+                { "user1", "pass456" },
+                { "user2", "qwerty" },
+                { "triani", "trianipm" }
+            };
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            // Input for username
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            // Input for password
+        }
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            // Checkbox to show/hide password
+            if (checkBox1.Checked)
+            {
+                textPassword.PasswordChar = '\0'; // Show password characters
+                textPassword.UseSystemPasswordChar = false;
+            }
+            else
+            {
+                textPassword.PasswordChar = '*'; // Hide password characters
+                textPassword.UseSystemPasswordChar = true;
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Clear login inputs
+            textUsername.Text = "";
+            textPassword.Text = "";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // Exit button to cancel login
+            Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Login button
+            string username = textUsername.Text;
+            string password = textPassword.Text;
+
+            if (ValidateLogin(username, password))
+            {
+                MessageBox.Show("Login successful!");
+                // Perform further actions after successful login
+            }
+            else
+            {
+                MessageBox.Show("Invalid username or password!");
+                // Handle invalid login attempt
+            }
+        }
+
+        private bool ValidateLogin(string username, string password)
+        {
+            // Design by contract: Precondition
+            if (string.IsNullOrWhiteSpace(username) || string.IsNullOrWhiteSpace(password))
+            {
+                throw new ArgumentException("Username and password must be provided!");
+            }
+
+            // Check if the entered username exists in the user credentials dictionary
+            if (userCredentials.ContainsKey(username))
+            {
+                // Check if the entered password matches the stored password for the username
+                if (userCredentials[username] == password)
+                {
+                    // Design by contract: Postcondition
+                    if (username != "triani" || password != "trianipm")
+                    {
+                        throw new Exception("Invalid postcondition: triani user should have trianipm");
+                    }
+
+                    return true; // Valid login
+                }
+            }
+
+            return false; // Invalid login
+        }
+    }
+}
