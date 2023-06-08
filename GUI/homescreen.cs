d1f67bfb514fe0663ef6d7a762fe1c1aa3c52ab7
hermwnS <126874286+hermwnS@users.noreply.github.com>
2023-06-08 20:00:22 +0700

    menambahkan GUi homescreen dan library kapasitas vending machine

---
+++ GUI/homescreen.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HermawanLibrary;
+
+namespace GUI
+{
+    public partial class homescreen : Form
+    {
+        public homescreen()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_MouseClick(object sender, MouseEventArgs e)
+        {
+            string input = textBox1.Text;
+            switch (input)
+            {
+                case "1":
+                    Console.WriteLine("jenis sampah");
+                    break;
+                case "2":
+                    Console.WriteLine("edit profil");
+                    break;
+                case "3":
+                    Console.WriteLine("lihat poin");
+                    break;
+                case "4":
+                    Kapasitas.periksaKapasitas<int>();
+                    break;
+            }
+        }
+    }
+}
---
+++ HermawanLibrary/Kapasitas.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HermawanLibrary
+{
+    public class Kapasitas
+    {
+        public static T periksaKapasitas<T>()
+        {
+            dynamic Isi = 0;
+            int Max = 6;
+
+            try
+            {
+                string Sampah = Console.ReadLine();
+                int SampahInt = Convert.ToInt32(Sampah);
+                if (SampahInt <= Max)
+                {
+                    Isi++;
+                }
+                else
+                {
+                    Console.WriteLine("Kapasitas sudah penuh");
+                }
+            }catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return Isi;
+        }
+    }
+}
