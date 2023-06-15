6533f9803604d1e45cdf440b82293fc3929037cc
Triani Putri Mumpuni <127008970+trianipm@users.noreply.github.com>
2023-06-15 11:38:22 +0700

    Melihat dan Menghitung Poin Dari Sampah CLO4
    Menggunakan teknik konstruksi runtime configuration

---
+++ MenghitungPoin.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace GUI
+{
+    partial class MenghitungPoin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(21, 222);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Alumunium";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.label1.Font = new System.Drawing.Font("MS Reference Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(92, 107);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 60);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "0";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(102, 222);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Plastic";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(183, 222);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Glass";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(58, 261);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Paper";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(139, 261);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Cardboard";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(191, 374);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(67, 22);
+            this.button6.TabIndex = 11;
+            this.button6.Text = "Exit";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(35, 374);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(67, 22);
+            this.button7.TabIndex = 10;
+            this.button7.Text = "Clear";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(31, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(178, 20);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "The total points you get!\r\n";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(24, 25);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(158, 31);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Total Points";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(27, 315);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(252, 16);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Thank you for disposing of trash properly!";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // MenghitungPoin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Lime;
+            this.ClientSize = new System.Drawing.Size(291, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "MenghitungPoin";
+            this.Text = "MenghitungPoin";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
---
+++ MenghitungPoin.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Contracts;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace GUI
+{
+    public partial class MenghitungPoin : Form
+    {
+        private Dictionary<string, int> garbagePoints; // Deklarasi variabel garbagePoints
+
+        private void UpdatePointsLabel()
+        {
+            int totalPoints = 0;
+
+            // Menghitung total poin
+            foreach (var kvp in garbagePoints)
+            {
+                totalPoints += kvp.Value;
+            }
+
+            // Memperbarui label dengan total poin
+            label1.Text = $"{totalPoints}";
+        }
+        private string configFilePath = "MenghitungSampah.json";
+        private Dictionary<string, int> LoadGarbagePoints()
+        {
+            string json = "";
+
+            try
+            {
+                if (File.Exists(configFilePath))
+                {
+                    json = File.ReadAllText(configFilePath);
+                    return JsonConvert.DeserializeObject<Dictionary<string, int>>(json);
+                }
+                else
+                {
+                    return GetDefaultGarbagePoints();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error reading config file: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            return GetDefaultGarbagePoints();
+        }
+        private void SaveGarbagePoints()
+        {
+            try
+            {
+                string json = JsonConvert.SerializeObject(garbagePoints, Formatting.Indented);
+                File.WriteAllText(configFilePath, json);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error saving config file: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            base.OnFormClosing(e);
+            SaveGarbagePoints();
+        }
+        private Dictionary<string, int> GetDefaultGarbagePoints()
+        {
+            return new Dictionary<string, int>
+            {
+                { "aluminum", 0 },
+                { "plastic", 0 },
+                { "glass", 0 },
+                { "paper", 0 },
+                { "cardboard", 0 }
+            };
+        }
+        private void AddPoints(string garbageType)
+        {
+            Contract.Requires(garbageType != null, "garbageType cannot be null");
+
+            if (garbagePoints.ContainsKey(garbageType))
+            {
+                // Jika jenis sampah ada dalam kamus, tambahkan poin
+                garbagePoints[garbageType] += 5;
+                UpdatePointsLabel();
+            }
+            else
+            {
+                MessageBox.Show("Jenis sampah tidak valid!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void ClearPoints()
+        {
+            foreach (var garbageType in garbagePoints.Keys.ToList())
+            {
+                garbagePoints[garbageType] = 0;
+            }
+
+            UpdatePointsLabel();
+        }
+        public MenghitungPoin()
+        {
+            InitializeComponent();
+            garbagePoints = LoadGarbagePoints();
+            UpdatePointsLabel();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // jenis sampah alumunium
+            AddPoints("aluminum");
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // jenis sampah plastic
+            AddPoints("plastic");
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // jenis sampah glass
+            AddPoints("glass");
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            // jenis sampah paper
+            AddPoints("paper");
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            // jenis sampah cardboard
+            AddPoints("cardboard");
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+            // hasil point yang dihasilkan dari setiap sampah yang masuk,
+            // dengan setiap jenis sampah yang masuk bertambah 5 point 
+        }
+        private void label2_Click(object sender, EventArgs e)
+        {
+            // merupakan hanya tulisan "The total points you get!"
+        }
+        private void label3_Click(object sender, EventArgs e)
+        {
+            // merupakan hanya tulisan "Total Points"
+        }
+        private void label4_Click(object sender, EventArgs e)
+        {
+            // merupakan hanya tulisan "Thank you for disposing of trash properly!"
+        }
+        private void button7_Click(object sender, EventArgs e)
+        {
+            // kalimat clear untuk me-reset total poin yang sudah didapatkan
+            ClearPoints();
+        }
+        private void button6_Click(object sender, EventArgs e)
+        {
+            // kelimat exit untuk keluar dari tampilan
+            Close();
+        }
+    }
+}
