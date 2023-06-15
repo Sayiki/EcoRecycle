fdf4ae8831d1382e8eaad01635f60f377a1bae06
Triani Putri Mumpuni <127008970+trianipm@users.noreply.github.com>
2023-06-15 22:31:15 +0700

    Login teknik table driven clo4

--- Login.Designer.cs
+++ Login.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace GUI
+﻿namespace GUI_tubes_KPL
 {
     partial class Login
     {
@@ -28,141 +28,118 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
             this.textUsername = new System.Windows.Forms.TextBox();
             this.textPassword = new System.Windows.Forms.TextBox();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnlogin = new System.Windows.Forms.Button();
+            this.btnregis = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(27, 31);
+            this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(156, 53);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(89, 31);
+            this.label1.Size = new System.Drawing.Size(53, 19);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Log In";
+            this.label1.Text = "LogIn";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(29, 62);
+            this.label2.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(95, 72);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(215, 20);
+            this.label2.Size = new System.Drawing.Size(181, 14);
             this.label2.TabIndex = 1;
-            this.label2.Text = "Please log in to your account!";
+            this.label2.Text = "Please Login Into Your Account!\r\n";
             this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // textUsername
+            // 
+            this.textUsername.Font = new System.Drawing.Font("Arial Narrow", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textUsername.Location = new System.Drawing.Point(131, 113);
+            this.textUsername.Name = "textUsername";
+            this.textUsername.Size = new System.Drawing.Size(160, 20);
+            this.textUsername.TabIndex = 2;
+            // 
+            // textPassword
+            // 
+            this.textPassword.Font = new System.Drawing.Font("Arial Narrow", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textPassword.Location = new System.Drawing.Point(131, 154);
+            this.textPassword.Name = "textPassword";
+            this.textPassword.Size = new System.Drawing.Size(160, 20);
+            this.textPassword.TabIndex = 3;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(30, 107);
+            this.label3.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(59, 115);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(70, 16);
-            this.label3.TabIndex = 2;
+            this.label3.Size = new System.Drawing.Size(66, 15);
+            this.label3.TabIndex = 4;
             this.label3.Text = "Username";
             this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(30, 171);
+            this.label4.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(59, 156);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(67, 16);
-            this.label4.TabIndex = 3;
+            this.label4.Size = new System.Drawing.Size(65, 15);
+            this.label4.TabIndex = 5;
             this.label4.Text = "Password";
-            this.label4.Click += new System.EventHandler(this.label4_Click);
             // 
-            // textUsername
+            // btnlogin
             // 
-            this.textUsername.Location = new System.Drawing.Point(33, 134);
-            this.textUsername.Name = "textUsername";
-            this.textUsername.Size = new System.Drawing.Size(223, 20);
-            this.textUsername.TabIndex = 4;
-            this.textUsername.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.btnlogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnlogin.ForeColor = System.Drawing.Color.Green;
+            this.btnlogin.Location = new System.Drawing.Point(131, 220);
+            this.btnlogin.Name = "btnlogin";
+            this.btnlogin.Size = new System.Drawing.Size(64, 23);
+            this.btnlogin.TabIndex = 7;
+            this.btnlogin.Text = "Login";
+            this.btnlogin.UseVisualStyleBackColor = true;
+            this.btnlogin.Click += new System.EventHandler(this.btnlogin_Click);
             // 
-            // textPassword
+            // btnregis
             // 
-            this.textPassword.Location = new System.Drawing.Point(33, 199);
-            this.textPassword.Name = "textPassword";
-            this.textPassword.Size = new System.Drawing.Size(223, 20);
-            this.textPassword.TabIndex = 5;
-            this.textPassword.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
-            // 
-            // checkBox1
-            // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(33, 225);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(94, 17);
-            this.checkBox1.TabIndex = 6;
-            this.checkBox1.Text = "See Password";
-            this.checkBox1.UseVisualStyleBackColor = true;
-            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
-            // 
-            // button1
-            // 
-            this.button1.BackColor = System.Drawing.Color.Gold;
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(80, 278);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(129, 31);
-            this.button1.TabIndex = 7;
-            this.button1.Text = "Log In";
-            this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(33, 367);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(67, 22);
-            this.button2.TabIndex = 8;
-            this.button2.Text = "Clear";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(189, 367);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(67, 22);
-            this.button3.TabIndex = 9;
-            this.button3.Text = "Exit";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.btnregis.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnregis.ForeColor = System.Drawing.Color.Green;
+            this.btnregis.Location = new System.Drawing.Point(201, 220);
+            this.btnregis.Name = "btnregis";
+            this.btnregis.Size = new System.Drawing.Size(90, 23);
+            this.btnregis.TabIndex = 8;
+            this.btnregis.Text = "Registrasi";
+            this.btnregis.UseVisualStyleBackColor = true;
+            this.btnregis.Click += new System.EventHandler(this.btnregis_Click);
             // 
             // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Lime;
-            this.ClientSize = new System.Drawing.Size(291, 450);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.checkBox1);
-            this.Controls.Add(this.textPassword);
-            this.Controls.Add(this.textUsername);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(357, 317);
+            this.Controls.Add(this.btnregis);
+            this.Controls.Add(this.btnlogin);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
+            this.Controls.Add(this.textPassword);
+            this.Controls.Add(this.textUsername);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.ForeColor = System.Drawing.Color.Black;
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Login";
             this.Text = "Login";
+            this.Load += new System.EventHandler(this.Login_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -172,13 +149,11 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
         private System.Windows.Forms.TextBox textUsername;
         private System.Windows.Forms.TextBox textPassword;
-        private System.Windows.Forms.CheckBox checkBox1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnlogin;
+        private System.Windows.Forms.Button btnregis;
     }
 }
--- Login.cs
+++ Login.cs
@@ -3,24 +3,25 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
-namespace GUI
+namespace GUI_tubes_KPL
 {
     public partial class Login : Form
     {
         private Dictionary<string, string> userCredentials; // Dictionary to store user credentials
-
         public Login()
         {
             InitializeComponent();
-
-            // Initialize user credentials
+            textPassword.PasswordChar = '*';
             InitializeUserCredentials();
+
+            this.FormClosing += login_FormClosing;
+
         }
 
         private void InitializeUserCredentials()
@@ -33,80 +34,6 @@ namespace GUI
                 { "user2", "qwerty" },
                 { "triani", "trianipm" }
             };
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-            // Merupakan hanya tulisan "Log In"
-        }
-
-        private void label2_Click(object sender, EventArgs e)
-        {
-            // Merupakan hanya tulisan "Please login to your akun"
-        }
-
-        private void label3_Click(object sender, EventArgs e)
-        {
-            // Merupakan hanya tulisan "Username"
-        }
-
-        private void label4_Click(object sender, EventArgs e)
-        {
-            // Merupakan hanya tulisan "Password"
-        }
-
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-            // Input for username
-        }
-
-        private void textBox2_TextChanged(object sender, EventArgs e)
-        {
-            // Input for password
-        }
-        private void checkBox1_CheckedChanged(object sender, EventArgs e)
-        {
-            // Checkbox to show/hide password
-            if (checkBox1.Checked)
-            {
-                textPassword.PasswordChar = '\0'; // Show password characters
-                textPassword.UseSystemPasswordChar = false;
-            }
-            else
-            {
-                textPassword.PasswordChar = '*'; // Hide password characters
-                textPassword.UseSystemPasswordChar = true;
-            }
-        }
-        private void button2_Click(object sender, EventArgs e)
-        {
-            // Clear login inputs
-            textUsername.Text = "";
-            textPassword.Text = "";
-        }
-
-        private void button3_Click(object sender, EventArgs e)
-        {
-            // Exit button to cancel login
-            Close();
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            // Login button
-            string username = textUsername.Text;
-            string password = textPassword.Text;
-
-            if (ValidateLogin(username, password))
-            {
-                MessageBox.Show("Login successful!");
-                // Perform further actions after successful login
-            }
-            else
-            {
-                MessageBox.Show("Invalid username or password!");
-                // Handle invalid login attempt
-            }
         }
 
         private bool ValidateLogin(string username, string password)
@@ -123,17 +50,78 @@ namespace GUI
                 // Check if the entered password matches the stored password for the username
                 if (userCredentials[username] == password)
                 {
-                    // Design by contract: Postcondition
-                    if (username != "triani" || password != "trianipm")
-                    {
-                        throw new Exception("Invalid postcondition: triani user should have trianipm");
-                    }
 
                     return true; // Valid login
                 }
             }
 
             return false; // Invalid login
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnlogin_Click(object sender, EventArgs e)
+        {
+            // Login button
+            string username = textUsername.Text;
+            string password = textPassword.Text;
+
+            if (ValidateLogin(username, password))
+            {
+                this.Hide();
+                MessageBox.Show("Login successful!");
+                dashboard db = new dashboard();
+                db.Show();
+            }
+            else
+            {
+                MessageBox.Show("Invalid username or password!");
+                // Handle invalid login attempt
+            }
+
+        }
+
+        private void btnregis_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            RegisterGUI rg = new RegisterGUI();
+            rg.Show();
+        }
+
+        private void login_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "sampahData.json");
+
+            // Delete the JSON file if it exists
+            if (File.Exists(filePath))
+            {
+                try
+                {
+                    File.Delete(filePath);
+                }
+                catch (IOException ex)
+                {
+                    MessageBox.Show("An error occurred while deleting the JSON file: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
